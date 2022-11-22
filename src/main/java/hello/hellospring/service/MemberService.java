5a2d043f6290f33fb1f990a6fa15e24b65a34b08
JeonSeongHun <shajh9708@naver.com>
2022-11-23 02:47:03 +0900

    comment

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,53 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+// ctrl + shift + t = test 메서드 자동 생성
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member) {
+
+        //같은 이름이 있는 중복 회원 제외
+        // ctrl + alt + m = 메서드를 빼서 따로 만들어줌
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long member) {
+        return memberRepository.findById(member);
+    }
+}
