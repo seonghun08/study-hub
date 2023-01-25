4e7af875092daa1e225a4d86a7d73c4129834c69
seonghun08 <shajh9708@naver.com>
2023-01-25 19:56:33 +0900

    ajax, js 게시판 화면 처리

---
+++ src/main/java/com/hun/book/springboot/web/IndexController.java
@@ -0,0 +1,37 @@
+package com.hun.book.springboot.web;
+
+import com.hun.book.springboot.dto.PostsListResponseDto;
+import com.hun.book.springboot.dto.PostsResponseDto;
+import com.hun.book.springboot.service.PostsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class IndexController {
+
+    private final PostsService postsService;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "posts-update";
+    }
+}
