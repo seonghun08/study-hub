04043fc156886fff92eb5a9244c10b525c87ae7b
seonghun08 <shajh9708@naver.com>
2023-01-26 22:38:08 +0900

    spring security 및 OAuth2.0 네이버 구글 로그인 구현

---
+++ src/main/java/com/hun/book/springboot/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.hun.book.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing // JPA auditing 활성화
+public class JpaConfig {
+}
