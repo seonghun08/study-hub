f887366b1b90251c9c2229cdde1b4ac12ea90a95
전성훈 <shajh9708@naver.com>
2023-03-03 00:51:09 +0900

    배열, 문자열 객체

--- js/example.js
+++ js/example.js
@@ -1,1 +1,9 @@
 document.write("환영합니다.<hr/>");
+
+var userName = prompt("당신의 영문 이름을 입력하세요.");
+var upperName = userName.toUpperCase();
+document.write(upperName, "<br/>");
+
+var userNum = prompt("당신의 연락처를 입력하세요.");
+var result = userNum.substring(0, userName.length - 4) + "****";
+document.write(result, "<br/>");
