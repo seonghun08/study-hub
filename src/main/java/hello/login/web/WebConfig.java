01fa1a0fd41274bc1d52dd0e098c6a616cee3446
전성훈 <shajh9708@naver.com>
2023-11-07 23:35:24 +0900

    스프링 인터셉터 & ArgumentResolver 활용 정리

--- src/main/java/hello/login/web/WebConfig.java
+++ src/main/java/hello/login/web/WebConfig.java
@@ -1,17 +1,46 @@
 package hello.login.web;
 
+import hello.login.web.argumentresolver.LoginMemberArgumentResolver;
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
+import hello.login.web.interceptor.LogInterceptor;
+import hello.login.web.interceptor.LoginCheckInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
+import java.util.List;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginMemberArgumentResolver());
+    }
+
+    /**
+     * 서블릿 필터와 스프링 인터셉터는 웹과 관련된 공통 관심사를 해결하기 위한 기술이다.
+     * 특별한 문제가 없다면 인터셉터를 사용하는 것이 좋다.
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1) // 우선순위 (낮을 수록 높음)
+                .addPathPatterns("/**") // "/" 내 하위 폴더 모두 인터셉터를 탈 수 있도록 설정
+                .excludePathPatterns("/css/**", "/*.ico", "/error"); // 인터셉터를 타지 않게 설정
+
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(2)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/members/add", "/login", "logout", "/css/**", "/*.ico", "/error");
+    }
+
+    // @Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter());
@@ -21,7 +50,7 @@ public class WebConfig {
         return filterRegistrationBean;
     }
 
-    @Bean
+    // @Bean
     public FilterRegistrationBean loginCheckFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LoginCheckFilter());
