8c83a3f3445f715a2bc9bf197c8f3052c71a3878
전성훈 <shajh9708@naver.com>
2023-11-14 23:02:56 +0900

    스프링이 제공하는 ExceptionResolver

--- src/main/java/hello/exception/api/ApiExceptionController.java
+++ src/main/java/hello/exception/api/ApiExceptionController.java
@@ -1,18 +1,20 @@
 package hello.exception.api;
 
+import hello.exception.exception.BadRequestException;
 import hello.exception.exception.UserException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
+@RequestMapping("/api")
 @RestController
 public class ApiExceptionController {
 
-    @GetMapping("/api/members/{id}")
+    @GetMapping("/members/{id}")
     public MemberDto getMember(@PathVariable("id") String id) {
 
         if (id.equals("ex")) {
@@ -28,6 +30,21 @@ public class ApiExceptionController {
         }
 
         return new MemberDto(id, "hello " + id);
+    }
+
+    @GetMapping("/response-status-ex1")
+    public String responseStatusEx1() {
+        throw new BadRequestException();
+    }
+
+    @GetMapping("/response-status-ex2")
+    public String responseStatusEx2() {
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error.bad", new IllegalArgumentException());
+    }
+
+    @GetMapping("/default-handler-ex")
+    public String defaultException(@RequestParam Integer data) {
+        return "ok";
     }
 
     @Data
--- src/main/java/hello/exception/resolver/UserHandlerExceptionResolver.java
+++ src/main/java/hello/exception/resolver/UserHandlerExceptionResolver.java
@@ -18,6 +18,12 @@ public class UserHandlerExceptionResolver implements HandlerExceptionResolver {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
+    /**
+     * @return
+     * - 빈 ModelAndView: 뷰를 렌더링 하지 않고, 정상 흐름으로 서블릿이 리턴된다.
+     * - ModelAndView: ModelAndView 에 View, Model 등의 정보를 지정해서 반환하면 뷰를 렌더링한다.
+     * - null: 다른 ExceptionResolver 를 찾아 실행한다. 만약 처리할 수 있는 ExceptionResolver 가 없는 경우 기존에 발생한 예외를 서블릿 밖으로 던진다.
+     */
     @Override
     public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
 
