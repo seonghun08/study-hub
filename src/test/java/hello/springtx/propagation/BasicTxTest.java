a0186eb476525c29592afd5753c0c1bf16514c86
전성훈 <shajh9708@naver.com>
2023-12-22 00:10:59 +0900

    트랜잭션 전파(propagation) 기본 이해

---
+++ src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -0,0 +1,45 @@
+package hello.springtx.propagation;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+import javax.sql.DataSource;
+
+@Slf4j
+@SpringBootTest
+public class BasicTxTest {
+
+    @Autowired
+    PlatformTransactionManager txManager;
+
+    @TestConfiguration
+    static class Config {
+
+        /**
+         * 스프링은 자동으로 transactionManager 를 빈으로 등록 해준다.
+         * 하지만 이처럼 수동 빈을 등록하는 이유는 개발자가 직접 transactionManager 를 직접 등록하기 위해서이다.
+         */
+        @Bean
+        public PlatformTransactionManager transactionManager(DataSource dataSource) {
+            return new DataSourceTransactionManager(dataSource);
+        }
+    }
+
+    @Test
+    void commit() {
+        log.info("트랜잭션 시작");
+        TransactionStatus status = txManager.getTransaction(new DefaultTransactionDefinition());
+
+        log.info("트랜잭션 커밋 시작");
+        txManager.commit(status);
+        log.info("트랜잭션 커밋 완료");
+    }
+}
