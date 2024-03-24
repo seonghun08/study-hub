d18a7abc39b7c4fb3dbb12107da1e50c52dd0bc7
전성훈 <shajh9708@naver.com>
2024-03-25 01:29:47 +0900

    AOP: retry, log 만들어보기

---
+++ src/main/java/hello/aop/exam/aop/RetryAspect.java
@@ -0,0 +1,32 @@
+package hello.aop.exam.aop;
+
+import hello.aop.exam.annotation.Retry;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+@Slf4j
+@Aspect
+public class RetryAspect {
+
+    // @Around("@annotation(hello.aop.exam.annotation.Retry)") -> @Around("@annotation(retry)") 인수로 받을 경우 대체 가능
+    @Around("@annotation(retry)")
+    public Object doRetry(ProceedingJoinPoint joinPoint, Retry retry) throws Throwable {
+        log.info("[retry] {} retry={}", joinPoint.getSignature(), retry);
+
+        int maxRetry = retry.value();
+        Exception exceptionHolder = null; // 예외 처리시 예외 정보를 담을 공간
+
+        for (int retryCount = 1; retryCount <= maxRetry; retryCount++) {
+            try {
+                log.info("[retry] try count={}/{}", retryCount, maxRetry);
+                return joinPoint.proceed();
+            } catch (Exception e) {
+                exceptionHolder = e;
+            }
+        }
+
+        throw exceptionHolder;
+    }
+}
