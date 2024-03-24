0986e634ad91f2fcdd34f07455d3fb064376fbf4
전성훈 <shajh9708@naver.com>
2024-03-25 00:59:19 +0900

    pointcut 총정리

--- src/main/java/hello/aop/member/MemberService.java
+++ src/main/java/hello/aop/member/MemberService.java
@@ -2,4 +2,6 @@ package hello.aop.member;
 
 public interface MemberService {
     String hello(String param);
+
+    Integer integer(int num);
 }
--- src/main/java/hello/aop/member/MemberServiceImpl.java
+++ src/main/java/hello/aop/member/MemberServiceImpl.java
@@ -4,17 +4,22 @@ import hello.aop.member.annotation.ClassAop;
 import hello.aop.member.annotation.MethodAop;
 import org.springframework.stereotype.Component;
 
-//@ClassAop
+@ClassAop
 @Component
 public class MemberServiceImpl implements MemberService {
 
     @Override
-//    @MethodAop("test value")
+    @MethodAop("test value")
     public String hello(String param) {
         return "ok";
     }
 
     public String internal(String param) {
         return "ok";
+    }
+
+    @Override
+    public Integer integer(int n) {
+        return n;
     }
 }
